e8ea16157218d6ff07e0d558800139a874985b3f
Leena <panchalleena8839@gmail.com>
2026-01-21 22:00:17 +0530

    Initial Spring Boot Project Setup

---
+++ src/test/java/com/example/resume/ResumeAiApplication/ResumeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resume.ResumeAiApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
