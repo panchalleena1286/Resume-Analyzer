e49dcd32e767c7a49aea08a97a6aec8885fb3639
Leena <panchalleena8839@gmail.com>
2026-01-23 21:58:07 +0530

    Initial Project setup

--- src/main/java/com/example/resumeai/entity/User.java
+++ src/main/java/com/example/resumeai/entity/User.java
@@ -2,4 +2,10 @@ package com.example.resumeai.entity;
 
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
 }
