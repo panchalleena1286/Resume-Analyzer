49c68b4963e1996d7f5eaef156b1608a8e8b7bdf
Leena <panchalleena8839@gmail.com>
2026-02-06 21:25:46 +0530

    Initial Commit

--- src/main/java/com/example/resumeai/entity/Resume.java
+++ src/main/java/com/example/resumeai/entity/Resume.java
@@ -3,4 +3,7 @@ package com.example.resumeai.entity;
 public class Resume {
 
     private String resume;
+    private String firstName;
+
+    private String CompanyName;
 }
--- src/main/java/com/example/resumeai/entity/User.java
+++ src/main/java/com/example/resumeai/entity/User.java
@@ -2,8 +2,6 @@ package com.example.resumeai.entity;
 
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
