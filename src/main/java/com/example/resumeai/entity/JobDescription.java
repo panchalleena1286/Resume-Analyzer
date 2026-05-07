02a3121d5a9656ce5e12c2fecea34aece3b9d617
Leena <panchalleena8839@gmail.com>
2026-05-07 22:15:49 +0530

    Initial Changes Commit

--- src/main/java/com/example/resumeai/entity/JobDescription.java
+++ src/main/java/com/example/resumeai/entity/JobDescription.java
@@ -1,10 +1,14 @@
 package com.example.resumeai.entity;
 
+import java.util.Date;
+
 public class JobDescription {
 
     private String desc;
 
     private String resumeName;
+
+    private Date date;
 
     private int resumenumber;
 
